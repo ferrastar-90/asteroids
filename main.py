1d899e21745f8256be555cca9f57eb337eed2e74
James Ferrari <james.ferrari@mac.com>
2026-06-24 01:57:24 +0100

    C: Update of main.py

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +15,11 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
